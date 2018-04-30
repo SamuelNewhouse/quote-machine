d1b0276c1b0374329ebcc7bb8659e79652f8b972
Samuel Newhouse <samuel_newhouse@yahoo.com>
2018-04-30 13:05:45 -0500

    add initial testing script

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,9 @@
 $(function () {
+  $("button").click(getQuote);
+
+  function getQuote(){
+    //WikiquoteApi.getRandomQuote("Mr. Sunshine (2011 TV series)", console.log, console.log );
+    WikiquoteApi.getRandomQuote().then(console.log, console.log);
+  }
 
 });
