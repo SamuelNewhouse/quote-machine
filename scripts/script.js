c15c05eca8bc776dfa757b77d59416fc167e9057
Samuel Newhouse <samuel_newhouse@yahoo.com>
2018-05-03 16:22:31 -0500

    add separate function for displaying quote and author

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,12 @@ $(function () {
 
   $("#new-quote").click(addQuote);
 
+  function setDisplay(info) {
+    $("#quote").html(info.quote);
+    $("#author").html('- ' + info.title);
+    $("#more-info h2").html('<a href="https://www.google.com/search?q=' + info.title + '" target="_blank">More Info</a>');
+  };
+
   function addQuote() {
     WikiquoteApi.getRandomQuote().then( (result) => {
       console.log(result);
