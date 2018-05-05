ba54cdf8c050ce86db0eb929fdd3ddfb4188b83e
Samuel Newhouse <samuel_newhouse@yahoo.com>
2018-05-05 11:13:12 -0500

    add function for hiding and unhiding arrows

--- scripts/script.js
+++ scripts/script.js
@@ -6,16 +6,29 @@ $(function () {
   $("#left-arrow").click(backOne);
   $("#right-arrow").click(forwardOne);
 
+  function updateArrows() {
+    if (quoteIndex == 0)
+      $("#left-arrow").css('visibility', 'hidden');
+    else
+      $("#left-arrow").css('visibility', 'visible');
+
+    if (quoteIndex == allQuotes.length - 1)
+      $("#right-arrow").css('visibility', 'hidden');
+    else
+      $("#right-arrow").css('visibility', 'visible');
+  };
+
   function setDisplay(info) {
     $("#quote").html(info.quote);
     $("#author").html('- ' + info.title);
     $("#more-info h2").html('<a href="https://www.google.com/search?q=' + info.title + '" target="_blank">More Info</a>');
+    updateArrows();
   };
 
   function addQuote() {
     WikiquoteApi.getRandomQuote().then(
       (success) => {
-        allQuotes.splice(quoteIndex + 1, 0, success);        
+        allQuotes.splice(quoteIndex + 1, 0, success);
         quoteIndex++;
         setDisplay(success);
       },
